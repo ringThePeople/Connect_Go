be79c70e9c104d210cd9b9b403bc845810dc50d1
JeongIn <uugg2922@gmail.com>
2017-04-06 21:19:46 +0900

    function: print_board

--- C_Go/C_Go/run.c
+++ C_Go/C_Go/run.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+void print_board(int board[][7]) {
+	int i,j;	//.¡Û¡Ü
+
+	for (i = 5; i >= 0; i--) {
+		for (j = 6; i >= 0; j++) {
+			if (board[i][j] == 0)
+				printf("%3s", " . ");
+			else if (board[i][j] == 1)
+				printf("%3s", "¡Ü");
+			else
+				printf("%3s", "¡Û");
+		}
+		printf("\n");
+	}
+
+}
 
 void main() {
 	int board[6][7] = { 0 }; //empty state :: 0 , player 1 :: 1 ,  player 2 :: 2

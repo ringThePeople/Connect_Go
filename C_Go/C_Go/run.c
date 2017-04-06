fdaaa07b26b98fb5c593493cb521e8109f970fcc
JeongIn <uugg2922@gmail.com>
2017-04-06 21:43:07 +0900

    start player

--- C_Go/C_Go/run.c
+++ C_Go/C_Go/run.c
@@ -4,16 +4,17 @@ void print_board(int board[][7]) {
 	int i,j;	//.∞≒
 
 	for (i = 5; i >= 0; i--) {
-		for (j = 6; i >= 0; j++) {
+		for (j = 6; j >= 0; j--) {
 			if (board[i][j] == 0)
-				printf("%3s", " . ");
+				printf("%2s", " .");
 			else if (board[i][j] == 1)
-				printf("%3s", "≒");
+				printf("%2s", "≒");
 			else
-				printf("%3s", "∞");
+				printf("%2s", "∞");
 		}
 		printf("\n");
 	}
+	printf("太0太1太2太3太4太5太6太");
 
 } // 04/06 21:23 jeongin 
 
@@ -21,6 +22,16 @@ void main() {
 	int board[6][7] = { 0 }; //empty state :: 0 , player 1 :: 1 ,  player 2 :: 2
 
 	int turn = 0;
+	
+	// --> 04/06 21:37 jeongin
+	int first, player1, player2;
+	printf("Who start first?");
+	scanf("%d", &turn);
+	// <-- 04/06 21:37 jeongin
+
+	print_board(board);
+
+	getchar();
 
 	
 }
